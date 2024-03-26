6ba5a7bb3c7441239b78788ed4ca6a0f410be4df
R. Broderick <programing@sanguinesoftware.com>
2024-03-26 00:00:22 -0400

    tox testing

--- tests/test_encryptedssgtringdeque.py
+++ tests/test_encryptedssgtringdeque.py
@@ -11,7 +11,7 @@ import pytest
 try:
     from Crypto.PublicKey import RSA  # nosec: B413
 except ModuleNotFoundError:
-    exit()
+    pytest.skip(allow_module_level=True)
 from stringdatadeque import EncryptedStringDeque
 from stringdatadeque import RSAMessage
 from stringdatadeque.encryptedstringdeque import Base64Encoded
